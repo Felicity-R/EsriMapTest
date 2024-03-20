b217dd946662efafb58bfcca4ea075a6f4c7dc1b
Felicity Rhone <frhone@latitudegeo.com>
2024-03-20 12:13:26 -0700

    Add location display

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Esri.ArcGISRuntime.Geometry;
-using Esri.ArcGISRuntime.Mapping;
+﻿using Esri.ArcGISRuntime.Mapping;
 using Map = Esri.ArcGISRuntime.Mapping.Map;
 
 namespace EsriMapTest
@@ -11,9 +10,15 @@ namespace EsriMapTest
         {
             InitializeComponent();
 
-            MyMapView.Map = new Map(new Basemap(new Uri("https://www.arcgis.com/home/item.html?id=ef5920f160bd4239bdeb1348de3a3156")));
-            MyMapView.Map.LoadAsync();
-            //MyMapView.LocationDisplay.IsEnabled = true;
+            Esri.ArcGISRuntime.ArcGISRuntimeEnvironment.ApiKey = "AAPKd746d17db8a248429fa96aa9b5188f98NPIrC5a1kIzxJgKfPwzhfRm65jEydZtfxTWrZWt0cK9U-YeGQOwWcfAgygIGntkT";
+
+            Task.Run(async () =>
+            {
+                MyMapView.Map = new Map(new Basemap(new Uri("https://www.arcgis.com/home/item.html?id=ef5920f160bd4239bdeb1348de3a3156")));
+                await MyMapView.Map.LoadAsync();
+
+                MyMapView.LocationDisplay.IsEnabled = true;
+            });
         }
     }
 
